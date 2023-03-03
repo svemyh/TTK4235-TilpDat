e2dc6d42b88c960784a0e779b78a14b291f7910d
Anton Tran <antontr@stud.ntnu.no>
2023-03-03 13:03:50 +0100

    så på stopButton og endret litt, ser mer senere2

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -2,7 +2,40 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
+#include <unistd.h>
 #include "driver/elevio.h"
+
+//#define N_FLOORS 4
+
+//typedef enum { 
+//    DIRN_DOWN   = -1,
+//    DIRN_STOP   = 0,
+//    DIRN_UP     = 1
+//} MotorDirection;
+
+
+//#define N_BUTTONS 3
+
+//typedef enum { 
+//    BUTTON_HALL_UP      = 0,
+//    BUTTON_HALL_DOWN    = 1,
+//    BUTTON_CAB          = 2:
+//} ButtonType;
+
+
+//void elevio_init(void);
+
+//void elevio_motorDirection(MotorDirection dirn);
+//void elevio_buttonLamp(int floor, ButtonType button, int value);
+//void elevio_floorIndicator(int floor);
+//void elevio_doorOpenLamp(int value);
+//void elevio_stopLamp(int value);
+
+//int elevio_callButton(int floor, ButtonType button);
+//int elevio_floorSensor(void);
+//int elevio_stopButton(void);
+//int elevio_obstruction(void);
+#define queue_size 100
 
 
 
@@ -34,6 +67,7 @@ int main(){
             }
         }
 
+        //STOP
         if(elevio_obstruction()){
             elevio_stopLamp(1);
         } else {
@@ -42,11 +76,15 @@ int main(){
         
         if(elevio_stopButton()){
             elevio_motorDirection(DIRN_STOP);
+            elevio_stopLamp(1);
+            if(floor != -1){
+                elevio_doorOpenLamp(1);
+            }
             break;
         }
-        
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
     }
 
+    sleep(3); // wait 3 sec
     return 0;
 }
